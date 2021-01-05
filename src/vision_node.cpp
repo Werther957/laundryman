86696e7c288be625bedb14abd19f0dbabac190bd
werther <werther957@gmail.com>
2021-01-05 14:02:58 +0100

    demo: cloth picture detection for classifcation verifying, to be improved for later use

--- src/vision_node.cpp
+++ src/vision_node.cpp
@@ -46,6 +46,7 @@
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/core.hpp"
 
 // Libtorch headers
 #include <torch/torch.h>
@@ -53,6 +54,97 @@
 
 //marker
 #include <geometry_msgs/PoseStamped.h>
+
+using namespace cv;
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+int thresh = 50, N = 11;
+static double angle( Point pt1, Point pt2, Point pt0 )
+{
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+// returns sequence of squares detected on the image.
+static void findSquares( const Mat& image, vector<vector<Point> >& squares )
+{
+    squares.clear();
+    Mat pyr, timg, gray0(image.size(), CV_8U), gray;
+    // down-scale and upscale the image to filter out the noise
+    pyrDown(image, pyr, Size(image.cols/2, image.rows/2));
+    pyrUp(pyr, timg, image.size());
+    vector<vector<Point> > contours;
+    // find squares in every color plane of the image
+    
+        int ch[] = {0, 0}; //can be {0,0} or {1,0} or {2,0}
+        mixChannels(&timg, 1, &gray0, 1, ch, 1);
+        // try several threshold levels
+        for( int l = 0; l < N; l++ )
+        {
+            //int l = 0; //l can be 0-11
+            // hack: use Canny instead of zero threshold level.
+            // Canny helps to catch squares with gradient shading
+            if( l == 0 )
+            {
+                // apply Canny. Take the upper threshold from slider
+                // and set the lower to 0 (which forces edges merging)
+               // Canny(gray0, gray, 0, thresh, 5);
+                Canny(gray0, gray, 85, 255, 5);
+                // dilate canny output to remove potential
+                // holes between edge segments
+                dilate(gray, gray, Mat(), Point(-1,-1));
+            }
+            else
+            {
+                // apply threshold if l!=0:
+                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                gray = gray0 >= (l+1)*255/N;
+            }
+            // find contours and store them all as a list
+            findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+            vector<Point> approx;
+            // test each contour
+            for( size_t i = 0; i < contours.size(); i++ )
+            {
+                // approximate contour with accuracy proportional
+                // to the contour perimeter
+                approxPolyDP(contours[i], approx, arcLength(contours[i], true)*0.02, true);
+                // square contours should have 4 vertices after approximation
+                // relatively large area (to filter out noisy contours)
+                // and be convex.
+                // Note: absolute value of an area is used because
+                // area may be positive or negative - in accordance with the
+                // contour orientation
+                
+                if( approx.size() == 4 &&
+                    fabs(contourArea(approx)) > 17500 && fabs(contourArea(approx)) < 25500 &&
+                    isContourConvex(approx) )
+                {
+                    double maxCosine = 0;
+                    std::cout << l << fabs(contourArea(approx)) << std::endl;
+                    for( int j = 2; j < 5; j++ )
+                    {
+                        // find the maximum cosine of the angle between joint edges
+                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+                    // if cosines of all angles are small
+                    // (all angles are ~90 degree) then write quandrange
+                    // vertices to resultant sequence
+                    if( maxCosine < 0.3 )
+                        squares.push_back(approx);
+                }
+            }
+        }
+    
+}
+/////////////////////////////////////////////
 
 class vision_node
 {
@@ -138,12 +230,18 @@ void vision_node::ImageProcessing()
 	if(i!=0)
 	{	
 
-	cv::imshow(win1, img_bgr);
+	//cv::imshow(win1, img_bgr);
 
     cv::Mat graymat;
     cv::cvtColor(img_bgr, graymat, cv::COLOR_BGR2GRAY);
 
     cv::imshow(win2, graymat);
+
+
+	vector<vector<Point> > squares;
+    findSquares(img_bgr, squares);
+    polylines(img_bgr, squares, true, Scalar(0, 255, 0), 3, LINE_AA);
+    imshow(win1, img_bgr);
 
 	// aruco marker detection
 
@@ -179,7 +277,7 @@ int main(int argc, char** argv)
 	ros::NodeHandle nh;
 	vision_node ts(nh);
 	//suscribe aruco marker from aruco ros
-    ros::Subscriber sub = nh.subscribe("/aruco_single/pose", 1000, pose_callback);
+    //ros::Subscriber sub = nh.subscribe("/aruco_single/pose", 1000, pose_callback);
 	ros::spin();
 }
 
